1e3b21a581b414df6582c34aa8509727e54b3764
arya-ht <aryaht23@gmail.com>
2017-07-18 22:43:16 -0500

    Fibonacci calculation
    This c program calulates the famous fibonacci of any number included in the .dat file up to the 50th term

---
+++ fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#pragma warning(disable:4996)
+// created by Arya HajiTaheri
+#define SIZE 10000
+int main() {
+	FILE *file;
+	int arr[SIZE];
+	int i = 0;
+	file = fopen("10000.dat", "r");
+	if (file == NULL) {
+		printf("Error: can't open file to read\n");
+		return 1;
+	}
+	while (!feof(file)
+		&& (i < SIZE))
+	{
+		fscanf(file, "%d", &(arr[i++]));
+	}
+	fclose(file);
+
+	unsigned long  int a, first = 0, next = 0;
+	printf("Enter the desired location of arr: to find fibonacci sequence:\n");
+	scanf("%d", &a);
+	unsigned long  int second = arr[a - 1];
+	for (int j = 0; j < 50; j++) {
+		next = second + first;
+		first = second;
+		second = next;
+	}									
+	printf("Answer: %lu\n", next);
+	system("pause");
+	return 0;
+}
